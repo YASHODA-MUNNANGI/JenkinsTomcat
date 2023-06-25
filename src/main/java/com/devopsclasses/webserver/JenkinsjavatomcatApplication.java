e470048085a50ceedecf5d7f89cdd612bd899790
YASHODA-MUNNANGI <117573227+YASHODA-MUNNANGI@users.noreply.github.com>
2023-06-25 13:04:38 +0530

    Project

---
+++ src/main/java/com/devopsclasses/webserver/JenkinsjavatomcatApplication.java
@@ -0,0 +1,13 @@
+package com.devopsclasses.webserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsjavatomcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsjavatomcatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devopsclasses/webserver/JenkinsjavatomcatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devopsclasses.webserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsjavatomcatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
